eb7e9b44ed2a649ec0755e322b7a7d064870289d
Parker P <exxcaelum@gmail.com>
2016-06-06 17:37:11 -0600

    added validation to property and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ActiveRecord::Base
+  has_many :properties
+
+  validates :title, presence: true,
+                    uniqueness: true
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,15 @@
 class Property < ActiveRecord::Base
   belongs_to :category
+
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :description, presence: true
+  validates :price, presence: true,
+                    numericality: true
+  validates :image_file_name, presence: true
+  validates :image_content_type, presence: true
+  validates :image_file_size, presence: true
+  validates :image_updated_at, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
 end
