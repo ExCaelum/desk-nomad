38e471fbc0c590f0a9eeee17a4eba7caa0f13a4b
Nick Pisciotta <npisciotta@gmail.com>
2016-06-07 17:28:48 -0600

    Add depth and scope to model and feature test Added deeper scope to feature testing and a dynamic create_property method. Added test for slug

--- spec/features/visitor_views_all_properties_spec.rb
+++ spec/features/visitor_views_all_properties_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 RSpec.feature "Visitor views all properties" do
   scenario "All properties are listed" do
-    create_property
-    create_second_property
+    num = create_property(2)
 
     visit properties_path
 
-    expect(page).to have_content("All Properties")
-    expect(page).to have_content("Property1")
-    expect(page).to have_content("Another Property")
+    within('h1') do
+      expect(page).to have_content("All Properties")
+    end
+
+    within('.col-md-3:first') do
+      expect(page).to have_content("Property0")
+    end
+
+    within('.col-md-3:last') do
+     expect(page).to have_content("Property1")
+   end
   end
 end
