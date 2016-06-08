71d72f453d665eaaaf31bfa93e39ffd1a6610ac3
Parker P <exxcaelum@gmail.com>
2016-06-08 13:59:51 -0600

    root styled, prop index styled, prop show semi-styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "root#show"
 
-  resources :properties, only: [:index]
+  resources :properties, only: [:index, :show]
 
   get "/:slug", to: "categories#show", as: 'category'
 end
