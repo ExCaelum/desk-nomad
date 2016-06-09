0575a2a75da565bf08e36d1811ea03d14dd03dae
Parker P <exxcaelum@gmail.com>
2016-06-08 20:03:16 -0600

    added ability to remove items from cart

--- app/controllers/cart_properties_controller.rb
+++ app/controllers/cart_properties_controller.rb
@@ -14,5 +14,12 @@ class CartPropertiesController < ApplicationController
     # @properties = @cart.return_properties
   end
 
+  def destroy
+    property = Property.find(params[:prop_id])
+    @cart.remove_property(property.id)
+    flash[:success] = "Successfully removed #{view_context.link_to property.title, property_path(property)} from your cart."
+    redirect_to cart_path
+  end
+
 
 end
