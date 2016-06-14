30c3330a83557e9fc39e29bdce17a481e5478271
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-14 14:56:23 -0600

    spec refactor user can view details checkout

--- spec/features/user_can_view_details_of_orders_spec.rb
+++ spec/features/user_can_view_details_of_orders_spec.rb
@@ -2,23 +2,20 @@ require "rails_helper"
 
 RSpec.feature "User sees details for an order" do
   scenario "User sees details about a specific order" do
-    create_user
-    category = create_category
+    user = create_user
+
     property0, property1 = create_property(2)
-    category.properties << property0
-    category.properties << property1
 
     visit "/properties"
     click_link "Property0"
     expect(current_path).to eq(property_path(property0))
     click_link "Book It!"
     expect(page).to have_content("You have booked 1 space.")
-    click_link "Sign In"
-    expect(current_path).to eq(login_path)
 
-    fill_in "Username", with: "turing123"
-    fill_in "Password", with: "password"
-    click_on "Login"
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit dashboard_path
+
     expect(page).to have_content("Roger")
     click_link "Bookings"
     expect(current_path).to eq("/cart")
