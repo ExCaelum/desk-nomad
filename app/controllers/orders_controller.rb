1e54725cce668a2f9e3ab78be438bcaeac83af04
Anna Weisbrodt <aweisbro@gmail.com>
2016-06-14 09:01:37 -0600

    refactor orders controller before action for current user check remove instance variable @order from create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :current_user_matches_order, only: [:show]
 
   def index
     if current_user
       @orders = current_user.orders
     else
       flash.now[:error] = "Permission Denied"
-      redirect_to "/"
+      redirect_to root_path
     end
   end
 
   def create
-    @order = current_user.orders.new
-    if @cart.has_contents
-      @order.save
-      @order.confirm_order(@cart)
+    order = current_user.orders.new
+     if @cart.has_contents
+      order.save
+      order.confirm_order(@cart)
       session.delete :cart
       flash[:success] = "Order was successfully placed"
       redirect_to orders_path
@@ -25,7 +26,13 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    if current_user.id == @order.user_id
+  end
+
+ private
+
+  def current_user_matches_order
+    order = Order.find(params[:id])
+    if current_user.id == order.user_id
       true
     else
       render file: "/public/404"
