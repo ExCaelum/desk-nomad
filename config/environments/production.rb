66e78bfd9c44beecced190dae7aed3b0728f27ad
Nick Pisciotta <npisciotta@gmail.com>
2016-06-15 10:18:40 -0600

    Add sendgrid to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,26 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+}
+
+ActionMailer::Base.smtp_settings = {
+:user_name => 'nickpisciotta',
+:password => 'awestrocks1',
+:domain => 'https://desk-nomad.herokuapp.com/',
+:address => 'smtp.sendgrid.net',
+:port => 587,
+:authentication => :plain,
+:enable_starttls_auto => true
+}
+
+
+
 end
