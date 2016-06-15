06e83a46b9394fee7834c8a5fe77d5f103880b8a
Parker P <exxcaelum@gmail.com>
2016-06-15 15:42:41 -0600

    removing some console.logs

--- app/assets/javascripts/properties.js
+++ app/assets/javascripts/properties.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   var $properties = $('.property');
 
   $("#property_filter_category").on("change", function () {
-    console.log("string")
     var currentCategory = this.value;
     $properties.each(function (index, property) {
     var $property = $(property);
