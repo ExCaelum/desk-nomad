9499ba49a3562653f29f24025c2677f70cb6885d
Parker P <exxcaelum@gmail.com>
2016-06-16 11:46:18 -0600

    fixed the contact mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,14 +5,7 @@ class ContactsController < ApplicationController
   end
 
   def create
-    @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = "Thank you for you space submission. We will contact you soon!"
-    else
-      flash.now[:error] = "Cannot send message"
-      render :new
-    end
+    redirect_to root_path
   end
 
 end
